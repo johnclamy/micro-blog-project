a47147c4e47bf1993e9b6b4c2717f372edd2b9a5
johnclamy <johnclamy@gmail.com>
2026-09-05 13:28:41 +0100

    set a list of navbar links

---
+++ frontend/src/components/layouts/NavLinks.tsx
@@ -0,0 +1,11 @@
+import type NavLink from '../../interfaces/navlink'
+
+
+const NavLinks: NavLink[] = [
+    { label: 'Posts', href: '/posts' },
+    { label: 'Search', href: '/search' },
+    { label: 'Profile', href: '/profile' },
+]
+
+
+export default NavLinks
